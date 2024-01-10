f41f8c73a79cb247ced834691df048e2fee3baa8
maslincamihai2 <118688714+maslincamihai2@users.noreply.github.com>
2024-01-10 10:11:04 +0200

    GridView imagini din directorul de resurse

--- app/src/main/java/com/example/proiectmaslinca/Galerie.java
+++ app/src/main/java/com/example/proiectmaslinca/Galerie.java
@@ -2,8 +2,16 @@ package com.example.proiectmaslinca;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Galerie extends AppCompatActivity {
@@ -11,8 +19,29 @@ public class Galerie extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galerie);
 
         Intent intent = getIntent();
         String cuvantCheie = intent.getStringExtra("PARAMETRU_CAUTARE");
+
+        // Colectie imagini
+        Integer[] imageIds = {R.drawable.ic_launcher_background, R.drawable.ic_launcher_foreground};
+
+        // GridView unde vor fi afisate imaginile
+        GridView gridView = findViewById(R.id.gridView);
+
+        // Adapter ce contine colectia de imagini si layout pentru afisarea unei imagini din grid
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, imageIds);
+        gridView.setAdapter(adapter);
+
+        // Listener atingere imagine din grid
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int imageId = imageIds[position];
+                System.out.println(imageId);
+            }
+        });
     }
 }
